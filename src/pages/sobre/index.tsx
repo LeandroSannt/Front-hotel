8828bc0eb5aa7a370ae8d20b4de82cfbc410dd65
karolyna.assuncao@wapstore.com.br <lsn_slim@yahoo.com.br>
2023-09-01 21:45:36 -0300

    build

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,27 +1,20 @@
-import { Footer } from "../../components/footer";
-import { Form } from "../../components/form";
-import { Galery } from "../../components/galery";
-import { Header } from "../../components/header";
-import { GlobalStyle } from "../../styles/global";
-import {PhotosProvider} from '../../hooks/PhotosContext'
-import {useContext} from 'react'
+import { Footer } from '../../components/footer'
+import { Form } from '../../components/form'
+import { Galery } from '../../components/galery'
+import { Header } from '../../components/header'
+import { GlobalStyle } from '../../styles/global'
+import { PhotosProvider } from '../../hooks/PhotosContext'
+import { useContext } from 'react'
 import { PhotosContext } from '../../hooks/PhotosContext'
 
-
-
 export function Sobre() {
-
-  const news = useContext(PhotosContext)
-  console.log(news)
   return (
     <PhotosProvider>
-      <Header/>
+      <Header />
       <Galery />
-      <GlobalStyle/>
-      <Form/>
-      <Footer/>
+      <GlobalStyle />
+      <Form />
+      <Footer />
     </PhotosProvider>
-  );
+  )
 }
-
-
